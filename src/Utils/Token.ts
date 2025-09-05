d564de9ea02fecf464a1ec39380591d4ea18c149
ayusclg <freelancerayus@gmail.com>
2025-09-05 23:07:04 +0545

    JWT

---
+++ src/Utils/Token.ts
@@ -0,0 +1,46 @@
+import Jwt from "jsonwebtoken";
+import { user } from "./Interfaces";
+
+export function generateRefreshToken(USER: user): string {
+    const secret = process.env.REFRESH_TOKEN_SECRET!;
+    const expiry = process.env.REFRESH_TOKEN_EXPIRY;
+    const expiresIn = Number(expiry);
+
+    if (!secret) {
+      throw new Error("Token Not Found");
+    }
+    if (!expiry) {
+      throw new Error("Token Expiry Not Found");
+    }
+  return Jwt.sign(
+    {
+      id: USER.id,
+    },
+    secret,
+    {
+      expiresIn,
+    }
+  );
+}
+
+export function generateAccessToken(USER: user): string {
+    const secret = process.env.ACCESS_TOKEN_SECRET
+    const expiry = process.env.ACCESS_TOKEN_EXPIRY
+    const expiresIn = Number(expiry)
+
+    if (!secret) {
+        throw new Error("Token Not Found")
+    }
+    if (!expiry) {
+        throw new Error("Token Expiry Not Set")
+    }
+
+    return Jwt.sign({
+        id: USER.id,
+        email: USER.email,
+        name:USER.name
+    },
+        secret, {
+        expiresIn,
+    })
+}
