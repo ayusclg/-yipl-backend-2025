b0473893cc1276768359b99ec6f4717078a63566
ayusclg <freelancerayus@gmail.com>
2025-09-07 00:19:20 +0545

    Caching Get Request

--- src/Utils/Cache.ts
+++ src/Utils/Cache.ts
@@ -12,9 +12,11 @@ redis.on("error", () => {
 export const setCacheOrGet = async<T>(key:string, fetchFnc:()=>Promise<T>, ttl=300):Promise<T> => {
     const cacheData = await redis.get(key)
     if (cacheData != null) {
+        console.log("CacheHit")
         return JSON.parse(cacheData) as T
     }
     else {
+        console.log("CacheMissed")
         const freshData = await fetchFnc()
         redis.set(key,JSON.stringify(freshData), "EX",ttl )
         return freshData
